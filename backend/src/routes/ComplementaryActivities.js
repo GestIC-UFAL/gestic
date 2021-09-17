93df7f6acecc19a7491e0681c6b83f97282f3aec
Vangasse <vangasse@ic.ufal.br>
2021-09-17 12:14:28 -0300

    teste

--- backend/src/routes/ComplementaryActivities.js
+++ backend/src/routes/ComplementaryActivities.js
@@ -5,10 +5,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.put('/', verifyJWT, async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const result = await userService.update({
-            ...req.body, token: req.token, idInToken: req.userId});
+        const result = await complementaryService.insert({
+            ...req.body, token: req.token, ownerId: req.userId});
         res.status(200).json(result);
     } catch (err) {
         console.log(err);
@@ -21,32 +21,6 @@ router.put('/', verifyJWT, async (req, res) => {
 router.get('/', async (req, res) => {
     try {
         const result = await complementaryService.getAll({});
-        res.status(200).json(result);
-    } catch (err) {
-        console.log(err);
-        (err.code) ?
-            res.status(500).json({ message: "Server Internal Error." }) :
-            res.status(500).json({ message: err.message });
-    }
-})
-
-router.get('/:id?', verifyJWT, async (req, res) => {
-    try {
-        const result = await userService.getById({
-            ...req.params, token: req.token, idInToken: req.userId });
-        res.status(200).json(result);
-    } catch (err) {
-        console.log(err);
-        (err.code) ?
-            res.status(500).json({ message: "Server Internal Error." }) :
-            res.status(500).json({ message: err.message });
-    }
-})
-
-router.delete('/:id', verifyJWT, async (req, res) => {
-    try {
-        const result = await userService.deleteById({
-            ...req.params, token: req.token });
         res.status(200).json(result);
     } catch (err) {
         console.log(err);
--- backend/src/services/ComplementaryActivities.js
+++ backend/src/services/ComplementaryActivities.js
@@ -10,6 +10,7 @@ class ComplementaryAcivities extends Service {
 
     async insert({ token, ownerId, name, description,
         group, hours, start, end}) {
+        console.log(">>>>"+ownerId)
         try {
             await super.insert({ token });
             const complementaryActivity = await this.db.create({
@@ -27,40 +28,6 @@ class ComplementaryAcivities extends Service {
             throw err;
         }
     }
-
-    // async update ({
-    //     token,
-    //     id,
-    //     ownerId,
-    //     name = undefined,
-    //     code = undefined,
-    //     codeClassroom = undefined,
-    //     linkClassroom = undefined,
-    //     linkMeets = undefined,
-    //     linkWpp = undefined,
-    //     linkTel = undefined
-    // }) {
-    //     try {
-    //         let offer = await super.update({ token, ownerId, id });
-    //         await db.offer.update(
-    //             {
-    //                 name: name ? name : offer.name,
-    //                 code: code ? code : offer.code,
-    //                 codeClassroom: codeClassroom ? codeClassroom : offer.codeClassroom,
-    //                 linkClassroom: linkClassroom ? linkClassroom : offer.linkClassroom,
-    //                 linkMeets: linkMeets ? linkMeets : offer.linkMeets,
-    //                 linkWpp: linkWpp ? linkWpp : offer.linkWpp,
-    //                 linkTel: linkTel ? linkTel : offer.linkTel,
-    //             }, {
-    //                 where: { id }
-    //             }
-    //         );
-    //         offer = await db.offer.findByPk(id);
-    //         return offer;
-    //     } catch (err) {
-    //         throw err;
-    //     }
-    // }
 
     async deleteAll(token) {
         try {
